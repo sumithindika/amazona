6339cdb4df0bc3d07f848c7fd95ecc120b7454c0
Sumith Indika Weerasingh <tsumith44@gmail.com>
2022-06-10 14:51:53 +0530

    create-react-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  <a href='/'>amazona</a>
- 
+        <a href="/">amazona</a>
       </header>
       <main>add product</main>
     </div>
